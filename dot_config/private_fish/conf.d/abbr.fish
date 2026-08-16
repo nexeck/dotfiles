88bd2f9be13e461efce8fa2a9aa78a707ba9d086
Marcel Beck <marcel@beck.im>
2026-08-16 21:09:12 +0200

    feat(fish): guard abbreviations with interactive check and add new aliases

--- dot_config/private_fish/conf.d/abbr.fish
+++ dot_config/private_fish/conf.d/abbr.fish
@@ -1,16 +1,32 @@
-if command -qa bat
-    abbr cat bat
+if status is-interactive
+    if command -qa bat
+        abbr cat bat
+    end
+
+    if command -qa dust
+        abbr du dust
+    end
+
+    if command -qa eza
+        abbr -a ls 'eza'
+        abbr -a lg 'eza --long --all --header --git --git-repos'
+        abbr -a l 'eza --long --all --header'
+        abbr -a la 'eza --all'
+        abbr -a ll 'eza --long'
+        abbr -a lt 'eza --long --tree'
+    end
+
+    if command -qa bottom
+        abbr top btm
+        abbr htop btm
+    end
+
+    if command -qa viddy
+        abbr watch viddy
+    end
+
+    if command -qa ripgrep
+        abbr grep rg
+    end
 end
 
-if command -qa dust
-    abbr du dust
-end
-
-if command -qa eza
-   	abbr -a ls 'eza'
-   	abbr -a lg 'eza --long --all --header --git --git-repos'
-   	abbr -a l 'eza --long --all --header'
-   	abbr -a la 'eza --all'
-   	abbr -a ll 'eza --long'
-   	abbr -a lt 'eza --long --tree'
-end
